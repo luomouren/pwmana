95b38b2de263e0ed395a6561269c43ba2b0a58b9
Amy <luomouren@foxmail.com>
2016-12-01 13:28:15 +0800

    SpringMVC  + spring+ mybatis整合

--- src/main/java/com/zacamy/pwmana/model/shopping/ItemsCustom.java
+++
@@ -1,9 +0,0 @@
-package com.zacamy.pwmana.model.shopping;
-
-import java.io.Serializable;
-
-public class ItemsCustom extends Items implements Serializable{
-
-	private static final long serialVersionUID = 1L;
-
-}
